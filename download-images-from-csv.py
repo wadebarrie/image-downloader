7c1010a4818a9c318e51e6d36d3b495f9a5ac209
Wade Barrie <wade.a.barrie@gmail.com>
2020-10-05 20:04:10 -0700

    added 404 error handling and logging

--- download-images-from-csv.py
+++ download-images-from-csv.py
@@ -4,21 +4,28 @@ import urllib.request
 from csv import reader
 import os.path
 import os
+import logging
 
 csv_filename = "fp-freestanding-fridge.csv"
+LOG_FILENAME = '404-errors.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.ERROR)
 
 with open(csv_filename, 'r') as csv_file:
     n = 1  # starting point
     for line in reader(csv_file):
-        tgt_folder = line[0] 
-        if not os.path.exists(tgt_folder):
-            os.makedirs(tgt_folder)
-            n = 1  # restart n for new folder
-        if line[1] != '' and line[0] != '': 
-            filename = ''.join([line[0], '-', str(n), line[1][-4:]])
-            destination = os.path.join(tgt_folder, filename)
-            urllib.request.urlretrieve(line[1], destination) # I need to be able to skip files that return a 404 error
-            n += 1
-            print("Image saved for {0}".format(line[1]))
-        else:
-            print("No result for {0}".format(line[1]))  
+            tgt_folder = line[0] 
+            if not os.path.exists(tgt_folder):
+                os.makedirs(tgt_folder)
+                n = 1  # restart n for new folder
+            if line[1] != '' and line[0] != '': 
+                filename = ''.join([line[0], '-', str(n), line[1][-4:]])
+                destination = os.path.join(tgt_folder, filename)
+            try:            
+                urllib.request.urlretrieve(line[1], destination)
+            except:
+                print("404 error - Not found: {0}".format(line[1])) 
+                logging.error("404 error - Not found {0}".format(line[1]))
+            else:
+                urllib.request.urlretrieve(line[1], destination)
+                n += 1
+                print("Image saved for {0}".format(line[1]))
